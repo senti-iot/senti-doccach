925f17ba16d069c4ff1c309f729a0c2090c2cb51
Andrei <at@senti.dk>
2021-03-31 20:12:08 +0200

    Slack Hooks

--- scripts/service-restart.sh
+++ scripts/service-restart.sh
@@ -5,7 +5,36 @@ if [[ "$1" == "master" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/doccache/production
 	systemctl restart senti-doccache.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Doccache MASTER updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Senti Doc Cache updated",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Branch:*\n'$1'"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Travis",
+					"emoji": true
+				},
+				"value": "travis-link",
+				"url": "http://travis-ci.com/github/senti-iot/senti-doccache",
+				"action_id": "button-action"
+			}
+		}
+	]
+}' $2
 	echo
 	exit 0
 fi
@@ -14,7 +43,36 @@ if [[ "$1" == "dev" ]]; then
 	npm install --prefix /srv/nodejs/senti/services/doccache/development
 	systemctl restart senti-doccache-dev.service
 	# Senti Slack Workspace
-	curl -X POST -H 'Content-type: application/json' --data '{"text":"Senti Doccache DEV updated and restarted!"}' $2
+	curl -X POST -H 'Content-type: application/json' --data '{
+	"blocks": [
+		{
+			"type": "header",
+			"text": {
+				"type": "plain_text",
+				"text": "Senti Doc Cache updated",
+				"emoji": true
+			}
+		},
+		{
+			"type": "section",
+			"text": {
+				"type": "mrkdwn",
+				"text": "*Branch:*\n'$1'"
+			},
+			"accessory": {
+				"type": "button",
+				"text": {
+					"type": "plain_text",
+					"text": "Travis",
+					"emoji": true
+				},
+				"value": "travis-link",
+				"url": "http://travis-ci.com/github/senti-iot/senti-doccache",
+				"action_id": "button-action"
+			}
+		}
+	]
+}' $2
 	echo
 	exit 0
 fi
